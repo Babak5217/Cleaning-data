e000036bb8e8398ed6f04c93cd18ace9cdf5d6cd
Babak <56364534+Babak5217@users.noreply.github.com>
2020-01-05 23:09:34 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,21 @@
 ##the first downlod data set
-##download.file("https://d396qusza40orc.cloudfront.net",
-##             destfile = "./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-##              method = "curl")
+download.file("https://d396qusza40orc.cloudfront.net",
+             destfile = "./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+              method = "curl")
  library(dplyr)
-##unzip("C:\Users\mzars\Downloads/UCI HAR Dataset.zip")
+unzip("C:/Users/mzars/Downloads/UCI HAR Dataset.zip")
 ##read the data
               X_test <- read.table("C:/Users/mzars/Downloads/UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="")
                 
                y_test <- read.table("~/UCI HAR Dataset/test/y_test.txt", quote="\"", comment.char="")
-                 
+               subject_test <- read.table("~/UCI HAR Dataset/test/subject_test.txt", quote="\"", comment.char="")
+               colnames(subject_test)="subject"
                X_train <- read.table("~/UCI HAR Dataset/train/X_train.txt", quote="\"", comment.char="")
                  View(X_train)
               y_train <- read.table("~/UCI HAR Dataset/train/y_train.txt", quote="\"", comment.char="")
                  View(y_train)
+                 subject_train <- read.table("~/UCI HAR Dataset/train/subject_train.txt", quote="\"", comment.char="")
+                 colnames(subject_train)="subject"
                features <- read.table("~/UCI HAR Dataset/features.txt", quote="\"", comment.char="")
                  View(features)
               activity_labels <- read.table("~/UCI HAR Dataset/activity_labels.txt", quote="\"", comment.char="")
@@ -33,8 +36,8 @@
                 y_train[y_train=="5"]<- "STANDING"
                 y_train[y_train=="6"]<- "LAYING"
 ## merge x_test and y_test
-                testbind<-cbind(X_test,y_test)
-                trainbind<- cbind(X_train,y_train)
+                testbind<-cbind(subject_test,X_test,y_test)
+                trainbind<- cbind(subject_train ,X_train,y_train)
 ##Merges the training and the test sets to create one data set.(the first one of project)
                 databind<-rbind(testbind,trainbind)
 ##We constructed a new data frame (mean.std) by extracting variables containing
@@ -92,13 +95,11 @@
                                       databind$`angle(X,gravityMean)`,
                                       databind$`angle(Y,gravityMean)`,
                                       databind$`angle(Z,gravityMean)`,
-                                      databind$V1)
+                                      databind$V1,databind$subject)
+    
 ## spilit activitis
                 dataspilit<-split(mean.std,mean.std$databind.V1)
 ##calculated means of data for each type of activitiis
-
-                 Finallist<- data.frame( sapply(dataspilit$LAYING,mean), sapply(dataspilit$SITTING,mean),
-                                         sapply(dataspilit$STANDING,mean), sapply(dataspilit$WALKING,mean),
-                                         sapply(dataspilit$WALKING_DOWNSTAIRS,mean), sapply(dataspilit$WALKING_UPSTAIRS,mean))
-                 names(Finallist) = activity_labels$V2
-                 
+                Finallist<- mean.std%>% group_by(databind.subject,databind.V1)%>%
+summarise_all(list(mean))
+                 write.table(Finallist,"Finallist.txt", row.name=FALSE)
